ab94b51b1d5c676d9ceb1960bbb9878bfbd56abf
Steven Feldman <mr.stevenfeldman@gmail.com>
2018-07-25 10:24:13 -0700

    fix bad linting from Smarthtings IDI... how can it mess up spaces soo much

--- smartapps/rokusmartthings/roku-connect.src/roku-connect.groovy
+++ smartapps/rokusmartthings/roku-connect.src/roku-connect.groovy
@@ -15,259 +15,259 @@
  */
 
 definition(
-	name: "Roku (Connect)",
-	namespace: "RokuSmartThings",
-	author: "Leslie Drewery",
-	description: "Manage Roku Devices",
-	singleInstance: true,
-	category: "SmartThings Labs",
-	iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
-	iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
-	iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
+  name: "Roku (Connect)",
+  namespace: "RokuSmartThings",
+  author: "Leslie Drewery",
+  description: "Manage Roku Devices",
+  singleInstance: true,
+  category: "SmartThings Labs",
+  iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+  iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+  iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
 
 def urnPlayerLink = "urn:roku-com:device:player:1"
 
 preferences {
-	page(name: "deviceDiscovery", title: "Roku Device Setup", content: "deviceDiscovery")
+  page(name: "deviceDiscovery", title: "Roku Device Setup", content: "deviceDiscovery")
 }
 
 def deviceDiscovery() {
-	int refreshCount = !state.refreshCount ? 0 : state.refreshCount as int
-	state.refreshCount = refreshCount + 1
-	def refreshInterval = 5
-		
-	def options = [:]
-	def devices = getVerifiedDevices()
-	devices.each {
-		def values = it.value.ssdpUSN.split(':')
-		def value = it.value.name ?: "${values[1]}-${values[3]}"
-		def key = it.value.mac
-		options["${key}"] = value
-	}
+  int refreshCount = !state.refreshCount ? 0 : state.refreshCount as int
+  state.refreshCount = refreshCount + 1
+  def refreshInterval = 5
+    
+  def options = [:]
+  def devices = getVerifiedDevices()
+  devices.each {
+    def values = it.value.ssdpUSN.split(':')
+    def value = it.value.name ?: "${values[1]}-${values[3]}"
+    def key = it.value.mac
+    options["${key}"] = value
+  }
 
-	if (!state.subscribe) {
-		log.debug "subscribe :: ${refreshCount}"
-		ssdpSubscribe();
-	}
-	
-	// ssdp request every 25 seconds
-	if ((refreshCount % 5) == 0) {
-		ssdpDiscover()
-	}
-	
-	// setup.xml request every 5 seconds except on discoveries
-	if (((refreshCount % 1) == 0) &&
-			((refreshCount % 5) != 0)) {
-		log.debug "verifyDevices :: ${refreshCount}"
-		verifyDevices()
-	}
+  if (!state.subscribe) {
+    log.debug "subscribe :: ${refreshCount}"
+    ssdpSubscribe();
+  }
+  
+  // ssdp request every 25 seconds
+  if ((refreshCount % 5) == 0) {
+    ssdpDiscover()
+  }
+  
+  // setup.xml request every 5 seconds except on discoveries
+  if (((refreshCount % 1) == 0) &&
+      ((refreshCount % 5) != 0)) {
+    log.debug "verifyDevices :: ${refreshCount}"
+    verifyDevices()
+  }
 
-	return dynamicPage(name: "deviceDiscovery", title: "Discovery Started!", nextPage: "", refreshInterval: 5, install: true, uninstall: true) {
-		section("Please wait while we discover your Roku Devices. Discovery can take five minutes or more, so sit back and relax! Select your device below once discovered.") {
-			input "selectedDevices", "enum", required: false, title: "Select Devices (${options.size() ?: 0} found)", multiple: true, options: options
-		}
-	}
+  return dynamicPage(name: "deviceDiscovery", title: "Discovery Started!", nextPage: "", refreshInterval: 5, install: true, uninstall: true) {
+    section("Please wait while we discover your Roku Devices. Discovery can take five minutes or more, so sit back and relax! Select your device below once discovered.") {
+      input "selectedDevices", "enum", required: false, title: "Select Devices (${options.size() ?: 0} found)", multiple: true, options: options
+    }
+  }
 }
 
 def installed() {
-	log.debug "Installed with settings: ${settings}"
-	initialize()
+  log.debug "Installed with settings: ${settings}"
+  initialize()
 }
 
 def updated() {
-	log.debug "Updated with settings: ${settings}"
-	addDevices()
-	unsubscribe()
-	initialize()
+  log.debug "Updated with settings: ${settings}"
+  addDevices()
+  unsubscribe()
+  initialize()
 }
 
 def initialize() {
-	unsubscribe()
-	unschedule()
-	
-	state.subscribe = false
-	
-	if (selectedDevices) {
-		addDevices()
-	}
+  unsubscribe()
+  unschedule()
+  
+  state.subscribe = false
+  
+  if (selectedDevices) {
+    addDevices()
+  }
 
-	runEvery5Minutes("ssdpDiscover")
+  runEvery5Minutes("ssdpDiscover")
 }
 
 def uninstalled() {
-	removeChildDevices(getChildDevices())
+  removeChildDevices(getChildDevices())
 }
 
 private removeChildDevices(delete) {
-	delete.each {
-		deleteChildDevice(it.deviceNetworkId)
-	}
+  delete.each {
+    deleteChildDevice(it.deviceNetworkId)
+  }
 }
 
 void ssdpDiscover() {
-	log.debug "ssdpDiscover: roku:ecp"
-	sendHubCommand(new physicalgraph.device.HubAction("lan discovery roku:ecp", physicalgraph.device.Protocol.LAN))
+  log.debug "ssdpDiscover: roku:ecp"
+  sendHubCommand(new physicalgraph.device.HubAction("lan discovery roku:ecp", physicalgraph.device.Protocol.LAN))
 }
 
 void ssdpSubscribe() {
-	log.debug "ssdpSubscribe fired"
-	subscribe(location, "ssdpTerm.roku:ecp", ssdpHandler)
-	state.subscribe = true
+  log.debug "ssdpSubscribe fired"
+  subscribe(location, "ssdpTerm.roku:ecp", ssdpHandler)
+  state.subscribe = true
 }
 
 Map verifiedDevices() {
-	def devices = getVerifiedDevices()
-	def map = [:]
-	devices.each {
-		def value = it.value.name ?: "Roku ${it.value.ssdpUSN.split(':')[1][-3..-1]}"
-		def key = it.value.mac
-		map["${key}"] = value
-	}
-	map
+  def devices = getVerifiedDevices()
+  def map = [:]
+  devices.each {
+    def value = it.value.name ?: "Roku ${it.value.ssdpUSN.split(':')[1][-3..-1]}"
+    def key = it.value.mac
+    map["${key}"] = value
+  }
+  map
 }
 
 void verifyDevices() {
-	def devices = getDevices()
-	if (devices.size() == 0) {
-    	log.warn "No devices to verify"
+  def devices = getDevices()
+  if (devices.size() == 0) {
+      log.warn "No devices to verify"
     } else {
-	    log.debug "Verifying ${devices.size()} devices"    
+      log.debug "Verifying ${devices.size()} devices"    
     }
-	devices.each {
-		int port = convertHexToInt(it.value.deviceAddress)
-		String ip = convertHexToIP(it.value.networkAddress)
-		String host = "${ip}:${port}"
+  devices.each {
+    int port = convertHexToInt(it.value.deviceAddress)
+    String ip = convertHexToIP(it.value.networkAddress)
+    String host = "${ip}:${port}"
         log.debug "Veryifing ${host}"
-		sendHubCommand(new physicalgraph.device.HubAction(
-			"""GET ${it.value.ssdpPath} HTTP/1.1\r\nHOST: ${host}\r\n\r\n""",
-			physicalgraph.device.Protocol.LAN, host, [callback: deviceDescriptionHandler]))
-	}
+    sendHubCommand(new physicalgraph.device.HubAction(
+      """GET ${it.value.ssdpPath} HTTP/1.1\r\nHOST: ${host}\r\n\r\n""",
+      physicalgraph.device.Protocol.LAN, host, [callback: deviceDescriptionHandler]))
+  }
 }
 
 def getVerifiedDevices() {
-	log.debug "getVerifiedDevices"
-	getDevices()
+  log.debug "getVerifiedDevices"
+  getDevices()
 }
 
 def getDevices() {
-	if (!state.devices) {
-		state.devices = [:]
-	}
-	state.devices
+  if (!state.devices) {
+    state.devices = [:]
+  }
+  state.devices
 }
 
 def addDevices() {
-	deleteRemovedDevices()
-	log.debug "addDevices"
-	def devices = getDevices()
+  deleteRemovedDevices()
+  log.debug "addDevices"
+  def devices = getDevices()
 
-	selectedDevices.each { dni ->
-		def selectedDevice = devices.find { it.value.mac == dni }
-		def d
-		if (selectedDevice) {
-			d = getChildDevices()?.find {
-				it.deviceNetworkId == selectedDevice.value.mac
-			}
-		}
+  selectedDevices.each { dni ->
+    def selectedDevice = devices.find { it.value.mac == dni }
+    def d
+    if (selectedDevice) {
+      d = getChildDevices()?.find {
+        it.deviceNetworkId == selectedDevice.value.mac
+      }
+    }
 
-		if (!d) {
-			log.debug "Creating Roku Device with dni: ${selectedDevice.value} - ${selectedDevice.value.networkAddress} : ${selectedDevice.value.deviceAddress}"
-			addChildDevice("RokuSmartThings", "Roku", selectedDevice.value.mac, selectedDevice?.value.hub, [
-				"label": selectedDevice?.value?.name ?: "Roku - ${selectedDevice.value.mac}",
-				"data": [
-					"mac": selectedDevice.value.mac,
-					"ip": selectedDevice.value.networkAddress,
-					"port": selectedDevice.value.deviceAddress
-				]
-			])
-		}
-	}
+    if (!d) {
+      log.debug "Creating Roku Device with dni: ${selectedDevice.value} - ${selectedDevice.value.networkAddress} : ${selectedDevice.value.deviceAddress}"
+      addChildDevice("RokuSmartThings", "Roku", selectedDevice.value.mac, selectedDevice?.value.hub, [
+        "label": selectedDevice?.value?.name ?: "Roku - ${selectedDevice.value.mac}",
+        "data": [
+          "mac": selectedDevice.value.mac,
+          "ip": selectedDevice.value.networkAddress,
+          "port": selectedDevice.value.deviceAddress
+        ]
+      ])
+    }
+  }
 //    deleteRemovedDevices();
 }
 
 private deleteRemovedDevices(){
-	if(selectedDevices){
-		log.trace "selectedDevices ${selectedDevices}"
-		log.trace "getChildDevices() ${getChildDevices()}"
-		def delete = getChildDevices().findAll { !selectedDevices.contains(it.deviceNetworkId) }
-		log.trace "Remove Devices ${delete}"
-		if(delete){
-			delete.each {
-				deleteChildDevice(it.deviceNetworkId)
-			}
-		}
-	} else {
-		removeChildDevices(getChildDevices())
-	}
+  if(selectedDevices){
+    log.trace "selectedDevices ${selectedDevices}"
+    log.trace "getChildDevices() ${getChildDevices()}"
+    def delete = getChildDevices().findAll { !selectedDevices.contains(it.deviceNetworkId) }
+    log.trace "Remove Devices ${delete}"
+    if(delete){
+      delete.each {
+        deleteChildDevice(it.deviceNetworkId)
+      }
+    }
+  } else {
+    removeChildDevices(getChildDevices())
+  }
 }
 
 def parse(description) {
-	log.trace "parse : " + description
+  log.trace "parse : " + description
 }
 
 def ssdpHandler(evt) {
-	def description = evt.description
-	def hub = evt?.hubId
+  def description = evt.description
+  def hub = evt?.hubId
 
-	def parsedEvent = parseLanMessage(description)
-	parsedEvent << ["hub":hub]
-	log.debug "parsedEvent ${parsedEvent}"
-	
-	def devices = getDevices()
-	String ssdpUSN = parsedEvent.ssdpUSN.toString()
-	if (devices."${ssdpUSN}") {
-		log.debug "found device ssdpUSN ${ssdpUSN}"
-		def d = devices."${ssdpUSN}"
-		if (d.networkAddress != parsedEvent.networkAddress || d.deviceAddress != parsedEvent.deviceAddress) {
-			d.networkAddress = parsedEvent.networkAddress
-			d.deviceAddress = parsedEvent.deviceAddress
-			def child = getChildDevice(parsedEvent.mac)
-			if (child) {
-				child.sync(parsedEvent.networkAddress, parsedEvent.deviceAddress)
-			}
-		}
-	} else {
-		log.debug "new device ssdpUSN ${ssdpUSN}"
-		devices << ["${ssdpUSN}": parsedEvent]
-	}
+  def parsedEvent = parseLanMessage(description)
+  parsedEvent << ["hub":hub]
+  log.debug "parsedEvent ${parsedEvent}"
+  
+  def devices = getDevices()
+  String ssdpUSN = parsedEvent.ssdpUSN.toString()
+  if (devices."${ssdpUSN}") {
+    log.debug "found device ssdpUSN ${ssdpUSN}"
+    def d = devices."${ssdpUSN}"
+    if (d.networkAddress != parsedEvent.networkAddress || d.deviceAddress != parsedEvent.deviceAddress) {
+      d.networkAddress = parsedEvent.networkAddress
+      d.deviceAddress = parsedEvent.deviceAddress
+      def child = getChildDevice(parsedEvent.mac)
+      if (child) {
+        child.sync(parsedEvent.networkAddress, parsedEvent.deviceAddress)
+      }
+    }
+  } else {
+    log.debug "new device ssdpUSN ${ssdpUSN}"
+    devices << ["${ssdpUSN}": parsedEvent]
+  }
 }
 
 void deviceDescriptionHandler(physicalgraph.device.HubResponse hubResponse) {
-	def body = hubResponse.xml
-	def devices = getDevices()
-	def device = devices.find { it?.key?.contains(body?.device-info?.udn?.text()) }
-	if (device) {
-		log.debug "device verified"
-		device.value << [
-			name: body?.device-info?.user-device-name?.text(),
-			model: body?.device-info?.model-number?.text(),
-			serialNumber: body?.device-info?.serial-number?.text(),
-			verified: true
-		]
-	}
+  def body = hubResponse.xml
+  def devices = getDevices()
+  def device = devices.find { it?.key?.contains(body?.device-info?.udn?.text()) }
+  if (device) {
+    log.debug "device verified"
+    device.value << [
+      name: body?.device-info?.user-device-name?.text(),
+      model: body?.device-info?.model-number?.text(),
+      serialNumber: body?.device-info?.serial-number?.text(),
+      verified: true
+    ]
+  }
 }
 
 private Integer convertHexToInt(hex) {
-	Integer.parseInt(hex,16)
+  Integer.parseInt(hex,16)
 }
 
 private String convertHexToIP(hex) {
-	[
-		convertHexToInt(hex[0..1]),
-	 	convertHexToInt(hex[2..3]),
-	 	convertHexToInt(hex[4..5]),
-	 	convertHexToInt(hex[6..7])
-	].join(".")
+  [
+    convertHexToInt(hex[0..1]),
+    convertHexToInt(hex[2..3]),
+    convertHexToInt(hex[4..5]),
+    convertHexToInt(hex[6..7])
+  ].join(".")
 }
 
 private Boolean canInstallLabs() {
-	return hasAllHubsOver("000.011.00603")
+  return hasAllHubsOver("000.011.00603")
 }
 
 private Boolean hasAllHubsOver(String desiredFirmware) {
-	return realHubFirmwareVersions.every { fw -> fw >= desiredFirmware }
+  return realHubFirmwareVersions.every { fw -> fw >= desiredFirmware }
 }
 
 private List getRealHubFirmwareVersions() {
-	return location.hubs*.firmwareVersionString.findAll { it }
+  return location.hubs*.firmwareVersionString.findAll { it }
 }
