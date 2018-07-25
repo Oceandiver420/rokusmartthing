50501f02e0402aefdeb917912cf0abaa88a1f6fc
Steven Feldman <mr.stevenfeldman@gmail.com>
2018-07-25 02:26:31 -0700

    Rewrite of the roku virtual button's smart app

--- smartapps/rokusmartthings/roku-virtual-buttons.src/roku-virtual-buttons.groovy
+++ smartapps/rokusmartthings/roku-virtual-buttons.src/roku-virtual-buttons.groovy
@@ -1,7 +1,7 @@
 /**
  *  Roku Virtual Buttons
  *
- *  Copyright 2017 Sam Steele
+ *  Copyright 2018 Steven Feldman
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
@@ -12,24 +12,31 @@
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
  *  for the specific language governing permissions and limitations under the License.
  *
+ *  Code inspired by Sam Steele (https://github.com/c99koder/SmartThings)
+ * 
+ *  TODO: add key_sequence support.
+ *		- UI page to specify sequence
+ *		-  test speed?
  */
 
 definition(
 	name: "Roku Virtual Buttons",
 	namespace: "RokuSmartThings",
-	author: "Sam Steele",
-	description: "Creates virtual buttons to switch between apps / inputs on a Roku TV",
+	author: "Steven Feldman",
+	description: "Enables users to create virtual buttons to control a Roku TV",
 	category: "SmartThings Labs",
 	iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
 	iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
 	iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png"
 )
 
-// TODO(stevenfeldman): Add option to disable some buttons.
 preferences {
-	section("Title") {
-		input "roku","capability.mediaController", title: "Roku Device", multiple: false, required: true
+	page(name: "rokuPage", title:"Roku Selection", nextPage: "buttonPage", uninstall: true) {
+		section("Select your Roku") {
+			input name: "roku", type: "capability.mediaController", title: "Roku Device", multiple: false, required: true
+		}
 	}
+	page(name: "buttonPage")
 }
 
 def installed() {
@@ -41,60 +48,128 @@ def updated() {
 	initialize()
 }
 
-def initialize() {
-	subscribe(roku, "activityList", createButtons)
-	createButtons(null)
-	roku.getAllActivities()
+def buttonPage() {
+    log.debug "buttonPage"
+    dynamicPage(name: "buttonPage", title:"Button Selection", uninstall: false, install: true) {
+    	setAvailableButtons()
+        section("Select desired buttons") {
+        	state.button_pref.values().each() {
+				input name: it.id, type: "bool", title: it.label, required: false
+            }
+		}
+    }
 }
 
-def createButtons(evt) {
+/***
+ setAvailableButtons - generates a list of available roku buttons.
+ If there was an enabled button that is no longer availble, keep it in the list.
+ ***/
+def setAvailableButtons() {
+	def button_pref = [:]
+	def cur_button_pref = [:]
+    if (state.button_pref) {
+    	cur_button_pref = state.button_pref
+    }
+
+	// https://sdkdocs.roku.com/display/sdkdoc/External+Control+API#ExternalControlAPI-KeypressKeyValues
+	def keys = [
+    	"Wake Via Lan", "Power On", "Power Off", "Home", "Rev", "Fwd", "Play",
+    	"Select", "Left", "Right", "Down", "Up", "Back", "InstantReplay",
+        "Info", "Backspace", "Search", "Enter", "FindRemote",
+        "Volume Down", "Volume Mute", "Volume Up",
+        "Channel Up", "Channel Down", "Input Tuner",  "Input AV1",
+        "Input HDMI1", "Input HDMI2", "Input HDMI3", "Input HDMI4"
+    ]
+    keys.each{
+		def id = it.replaceAll(' ', '')
+		button_pref[id] = [
+			id: id,
+			label: it,
+            type: "key",
+			enabled: cur_button_pref.containsKey(id) && cur_button_pref[id].enabled
+		]
+	}
+
 	def activityList = roku.currentValue("activityList")
 	if (activityList != null) {
 		def appsNode = new XmlSlurper().parseText(activityList)
-
 		appsNode.children().each{
-			def appId = it.@id.toString()
-			def deviceLabel = it.text()
-			if (getChildDevice(appId) == null) {
-				def device = addChildDevice("RokuSmartThings", "Roku Button Tile", appId, null, [label: "Roku: $deviceLabel"])
-				state["$device.id"] = appId
-				log.debug "Created button tile $device.id for channel $deviceLabel ($appId)"
-			} else {
-				log.debug "Skipped $appId"
-			}
+            def id = it.@id.toString()
+            if (id == null) { return }
+			button_pref[id] = [
+				id: id,
+				label: it.text(),
+                type: "app",
+			  	enabled: cur_button_pref.containsKey(id) && cur_button_pref[id].enabled
+		  ]
 		}
 	}
-		
-	if (getChildDevice("powerOn") == null) {
-			def device = addChildDevice("RokuSmartThings", "Roku Button Tile", "powerOn", null, [label: "Roku: Power On"])
-			state["$device.id"] = "powerOn"
-			log.debug "Created Power On tile $device.id"
-	} else {
-			log.debug "Skipped Power On tile"
+	// Add any enabled buttons that are no longer valid. If the user deselects them,
+	// they will be removed from the list.
+	cur_button_pref.values().each{
+		if (it.enabled && !button_pref.containsKey(it.id)) {
+			button_pref[it.id] = it
+		}
 	}
+    state.button_pref = button_pref
+}
 
-	if (getChildDevice("powerOff") == null) {
-		def device = addChildDevice("RokuSmartThings", "Roku Button Tile", "powerOff", null, [label: "Roku: Power Off"])
-		state["$device.id"] = "powerOff"
-		log.debug "Created Power Off tile $device.id"
-	} else {
-		log.debug "Skipped Power Off tile"
-	}
+def initialize() {
+	// log.debug "List of options: ${state.button_pref}"
+    
+    // Iterate over the button options and update the enabled list.
+	state.button_pref.values().each() {
+        it.enabled = settings.get(it.id, false)
+		if (getChildDevice(it.id) == null && it.enabled) {
+        	def device = addChildDevice("RokuSmartThings", "Roku Button Tile", it.id, null, [label: "Roku: ${it.label}"])
+			state["$device.id"] = it.id
+			log.debug "Created button tile $device.id for channel ${it.label} (${it.id})"
+        }
+    }
 
+	// After adding new devices, go through and remove old ones and re-subscribe
 	getAllChildDevices().each {
+        def id = state[it.id]
+        // Ensure that disabled buttons are removed.
+    	if (!state.button_pref.get(id) || !state.button_pref.get(id).enabled) {
+        	log.debug "deleting $it (id=$id) because it is disabled."
+        	deleteChildDevice(id)
+            return
+        }
 		subscribe(it, "switch", switchHandler)
 	}
 }
 
 def switchHandler(evt) {
-	if (evt.value == "on") {
-		if (state["$evt.device.id"] == "powerOn") {
-			sendHubCommand(new physicalgraph.device.HubAction ("wake on lan ${roku.deviceNetworkId}", physicalgraph.device.Protocol.LAN, null, [:]))
-			roku.pressKey("Power")
-		} else if (state["$evt.device.id"] == "powerOff") {
-			roku.pressKey("PowerOff")
-		} else {
-			roku.launchAppId(state["$evt.device.id"])
-		}
+	// Only respond to 'on' events
+	if (evt.value != "on") {
+		log.warn "unexpected event: $evt.value"
+		return
 	}
+    // Get the device id and the pref.
+    def b = state.button_pref[state["$evt.device.id"]]
+    if (!b) {
+    	log.error "Event from button not in pref. $evt $id"
+    	return
+    }
+
+	if (b.id == "wakeViaLan") {
+		rokue.wakeViaLan()
+		return
+	}
+    if (b.type == "app") {
+    	roku.launchAppId(b.id)
+        return
+    }
+    if (b.type == "key") {
+    	roku.pressKey(b.id)
+        return
+    }
+    if (b.type == "key_sequence") {
+    	for (String key : b.keys) {
+        	roku.pressKey(key)
+        }
+        return
+    }
+    log.error "unknown type: $evt $id $b"
 }
